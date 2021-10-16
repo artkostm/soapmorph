f55a3d510aee1e921d883964d5aa8f7bd0ce3187
Artsiom Chuiko <artkostm@gmail.com>
2021-10-16 18:14:56 +0300

    update DataF

--- src/main/scala/com/artkostm/data/soap/client/schema/DataF.scala
+++ src/main/scala/com/artkostm/data/soap/client/schema/DataF.scala
@@ -1,5 +1,6 @@
 package com.artkostm.data.soap.client.schema
 
+
 import cats.implicits._
 import cats.{Applicative, Functor, Monad, Traverse}
 import com.artkostm.data.soap.client.schema.Avro.EnvT
@@ -10,6 +11,8 @@ import higherkindness.droste.data.{AttrF, Fix}
 import higherkindness.droste.syntax.all.toFixSyntaxOps
 import higherkindness.droste.util.DefaultTraverse
 
+import java.sql.{Date, Timestamp}
+import scala.annotation.tailrec
 import scala.language.higherKinds
 
 sealed trait DataF[A] {
@@ -27,13 +30,15 @@ sealed trait DataF[A] {
       elements.foldRight(Seq.empty[B].pure[F]) {
         case (e, acc) => (f(e), acc).mapN(_ +: _)
       }.map(GArray.apply)
-    case GDouble(n)        => (GDouble[B](n): DataF[B]).pure[F]
-    case GBoolean(n)       => (GBoolean[B](n): DataF[B]).pure[F]
-    case GFloat(n)         => (GFloat[B](n): DataF[B]).pure[F]
-    case GString(n)        => (GString[B](n): DataF[B]).pure[F]
-    case GLong(n)          => (GLong[B](n): DataF[B]).pure[F]
-    case GInt(n)           => (GInt[B](n): DataF[B]).pure[F]
-    case GNull()           => (GNull[B](): DataF[B]).pure[F]
+    case GDouble(n)  => (GDouble[B](n): DataF[B]).pure[F]
+    case GBoolean(n) => (GBoolean[B](n): DataF[B]).pure[F]
+    case GFloat(n)   => (GFloat[B](n): DataF[B]).pure[F]
+    case GString(n)  => (GString[B](n): DataF[B]).pure[F]
+    case GLong(n)    => (GLong[B](n): DataF[B]).pure[F]
+    case GInt(n)     => (GInt[B](n): DataF[B]).pure[F]
+    case GNull(t)     => (GNull[B](t): DataF[B]).pure[F]
+    case GDatetime(n) => (GDatetime[B](n): DataF[B]).pure[F]
+    case GDate(n)     => (GDate[B](n): DataF[B]).pure[F]
   }
 }
 
@@ -42,39 +47,64 @@ sealed trait GValue[A] extends DataF[A] {
 }
 
 object DataF {
+
+  def struct(fields: Map[String, Fix[DataF]]): Fix[DataF] =
+    GStruct(fields).fix[DataF]
+
   implicit class FixedData(d: Fix[DataF]) {
+    def put(name: String, value: Fix[DataF]): Fix[DataF] = d match {
+      case Fix(s: GStruct[Fix[DataF]]) => s.put(name, value).fix[DataF]
+      case x => throw new UnsupportedOperationException(s"Cannot put value $value to $x")
+    }
+
     def get(fieldName: String): Option[Any] = {
-      def search(dd: Fix[DataF]): Option[Fix[DataF]] = dd match {
-        case Fix(GStruct(fields))  => fields.get(fieldName).flatMap(search)
-        case f @ Fix(_: GValue[_]) => Some(f)
-        case _                     => None
+      @tailrec def search(dd: Option[Fix[DataF]]): Option[Fix[DataF]] = dd match {
+        case Some(Fix(GStruct(fields)))  => search(fields.get(fieldName))
+        case Some(f @ Fix(_: GValue[_])) => Some(f)
+        case _                           => None
       }
 
-      search(d).flatMap(Fix.un(_) match {
+      search(Some(d)).flatMap(Fix.un(_) match {
         case field: GValue[_] => Option(field.value)
         case _                => None
       })
+    }
+
+    def getField(fieldName: String): Fix[DataF] = {
+      @tailrec def search(dd: Option[Fix[DataF]]): Option[Fix[DataF]] = dd match {
+        case Some(Fix(GStruct(fields)))  => search(fields.get(fieldName))
+        case Some(f @ Fix(_: GValue[_])) => Some(f)
+        case _                           => None
+      }
+
+      search(Some(d)).getOrElse(nullF)
     }
   }
 
   def isNotNull(d: Fix[DataF]): Boolean =
     Fix.un(d) match {
-      case GNull() => false
-      case _       => true
+      case GNull(_) => false
+      case _        => true
     }
 
-  val nullF: Fix[DataF] = GNull().fix[DataF]
+  val nullF: Fix[DataF] = GNull("").fix[DataF]
 
-  final case class GStruct[A](fields: Map[String, A]) extends DataF[A]
-  final case class GArray[A](elements: Seq[A])        extends DataF[A]
-  final case class GString[A](value: String)          extends GValue[A]
-  final case class GBoolean[A](value: Boolean)        extends GValue[A]
-  final case class GDouble[A](value: Double)          extends GValue[A]
-  final case class GFloat[A](value: Float)            extends GValue[A]
-  final case class GInt[A](value: Int)                extends GValue[A]
-  final case class GLong[A](value: Long)              extends GValue[A]
+  def nullF(tpe: String): Fix[DataF] = GNull(tpe).fix[DataF]
 
-  final case class GNull[A]() extends GValue[A] {
+  final case class GStruct[A](fields: Map[String, A]) extends DataF[A] {
+    def put(name: String, value: A): GStruct[A] = GStruct[A](fields + (name -> value))
+  }
+  final case class GArray[A](elements: Seq[A])    extends DataF[A]
+  final case class GString[A](value: String)      extends GValue[A]
+  final case class GBoolean[A](value: Boolean)    extends GValue[A]
+  final case class GDouble[A](value: Double)      extends GValue[A]
+  final case class GFloat[A](value: Float)        extends GValue[A]
+  final case class GInt[A](value: Int)            extends GValue[A]
+  final case class GLong[A](value: Long)          extends GValue[A]
+  final case class GDatetime[A](value: Timestamp) extends GValue[A]
+  final case class GDate[A](value: Date)          extends GValue[A]
+
+  final case class GNull[A](tpe: String) extends GValue[A] {
     override val value: Any = null
   }
 
@@ -84,7 +114,7 @@ object DataF {
       case (structF @ Fix(StructF(fields, _)), Fix(GStruct(values))) =>
         AttrF
           .apply[DataF, Fix[SchemaF], (Fix[SchemaF], Fix[DataF])](
-            (structF, GStruct(values.map {
+            (structF, GStruct(values.filter(p => fields.contains(p._1)).map {
               case (name, value) => (name, (fields.apply(name), value))
             }))
           )
@@ -114,7 +144,9 @@ object DataF {
       case GFloat(v)        => GFloat[B](v)
       case GInt(v)          => GInt[B](v)
       case GLong(v)         => GLong[B](v)
-      case GNull()          => GNull[B]()
+      case GNull(t)          => GNull[B](t)
+      case GDatetime(v)     => GDatetime[B](v)
+      case GDate(v)         => GDate[B](v)
     }
   }
 }
